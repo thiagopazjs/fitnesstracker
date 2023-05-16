ae5b25bf394cf8a9b67702494a08ccf6619e43c0
Thiago Paz <tpazdev@gmail.com>
2023-05-16 15:47:43 -0300

    Add Function validate IMCActivity

--- app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
@@ -2,10 +2,34 @@ package com.thiagopaz.fitnesstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun validate(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+            && editHeight.text.toString().isNotEmpty()
+            && !editWeight.text.toString().startsWith("0")
+            && !editHeight.text.toString().startsWith("0"))
     }
 }
