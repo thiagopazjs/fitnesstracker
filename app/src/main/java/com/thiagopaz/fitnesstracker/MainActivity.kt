04f9a6dfc7ebd5e9f2bd0f16eb2a1c09d5776a83
Thiago Paz <tpazdev@gmail.com>
2023-05-16 10:34:05 -0300

    primeiro commit

---
+++ app/src/main/java/com/thiagopaz/fitnesstracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thiagopaz.fitnesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
