b22fa66eb4a7e79493cf0be625f6c3b34fae8532
Thiago Paz <tpazdev@gmail.com>
2023-05-16 12:22:47 -0300

    add ImcActivity

---
+++ app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
@@ -0,0 +1,11 @@
+package com.thiagopaz.fitnesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc)
+    }
+}
--- app/src/main/java/com/thiagopaz/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/thiagopaz/fitnesstracker/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.thiagopaz.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnImc: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnImc = findViewById(R.id.btn_imc)
+
+        btnImc.setOnClickListener {
+            //navegar prox tela
+            val i = Intent(this, ImcActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
