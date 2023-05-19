b9810606de2bcb752c4bbbfe0a9c55710390d112
Thiago Paz <tpazdev@gmail.com>
2023-05-19 11:42:23 -0300

    refactor add @Strings fun imcResp

--- app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 
 class ImcActivity : AppCompatActivity() {
 
@@ -36,16 +37,19 @@ class ImcActivity : AppCompatActivity() {
         }
     }
 
+
+
+    @StringRes
     private fun imcResponse(imc: Double): Int {
-        when {
-            imc < 15.0 -> return  R.string.imc_severely_low_weight
-            imc < 16.0 -> return  R.string.imc_very_low_weight
-            imc < 18.5 -> return  R.string.imc_very_low_weight
-            imc < 25.0 -> return  R.string.normal
-            imc < 30.0 -> return  R.string.imc_high_weight
-            imc < 35.0 -> return  R.string.imc_high_weight
-            imc < 40.0 -> return  R.string.imc_severely_high_weight
-            else -> return  R.string.imc_extreme_weight
+        return when {
+            imc < 15.0 -> R.string.imc_severely_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_very_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
         }
 
     }
