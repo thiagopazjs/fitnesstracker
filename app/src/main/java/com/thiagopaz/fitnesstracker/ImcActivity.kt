05894bc225f9b0e874fb8c82a655f4bfa4832adc
Thiago Paz <tpazdev@gmail.com>
2023-05-23 11:26:36 -0300

    Add hideSoftInputFromWindow

--- app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/com/thiagopaz/fitnesstracker/ImcActivity.kt
@@ -1,9 +1,11 @@
 package com.thiagopaz.fitnesstracker
 
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -44,6 +46,9 @@ class ImcActivity : AppCompatActivity() {
                 }
                 .create()
                 .show()
+
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken,0)
         }
     }
 
