2599295273889df3c69e36de5e662a419394236f
Thiago Paz <tpazdev@gmail.com>
2023-05-30 12:40:18 -0300

    feat: add onItemClickListener.invoke at Main

--- app/src/main/java/com/thiagopaz/fitnesstracker/MainActivity.kt
+++ app/src/main/java/com/thiagopaz/fitnesstracker/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.thiagopaz.fitnesstracker
 
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -25,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         mainItems.add(
             MainItem(
                 id = 1,
-                drawableId = R.drawable.baseline_directions_run_24,
+                drawableId = R.drawable.baseline_accessibility_24,
                 textStringId = R.string.imc,
                 color = Color.GREEN
             )
@@ -33,52 +36,82 @@ class MainActivity : AppCompatActivity() {
         mainItems.add(
             MainItem(
                 id = 2,
-                drawableId = R.drawable.baseline_accessibility_24,
+                drawableId = R.drawable.baseline_directions_run_24,
                 textStringId = R.string.tmb,
                 color = Color.YELLOW
             )
         )
 
 
-        val adapter = MainAdapter(mainItems)
+        val adapter = MainAdapter(mainItems) { id ->
+            // METODO 3: IMPL VIA FUNCTIONS
+            when (id) {
+                1 -> {
+                    val intent = Intent(this@MainActivity, ImcActivity::class.java)
+                    startActivity(intent)
+                }
+                2 -> {
+                    // abrir uma outra activity
+                }
+            }
+            Log.i("Teste", "clicou $id!!")
+        }
+
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
-        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.layoutManager = GridLayoutManager(this, 2)
 
+        // classe para administar a recyclerview e suas celulas (os seus layouts de itens)
+        // Adapter ->
     }
 
-    private inner class MainAdapter(private val mainItems: List<MainItem>) :
-        RecyclerView.Adapter<MainViewHolder>() {
-        // Qual o XML da celula especifica
+
+
+    private inner class MainAdapter(
+        private val mainItems: List<MainItem>,
+//        private val onItemClickListener: OnItemClickListener
+        private val onItemClickListener: (Int) -> Unit,
+    ) : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+        // 1 - Qual é o layout XML da celula especifica (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             return MainViewHolder(view)
-
         }
 
-        // quantidade de celulas  que terá na listagem
+        // 2 - disparado toda vez houver uma rolagem na tela e for necessario trocar o conteudo
+        // da celula
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
+        }
+
+        // 3 - informar quantas celulas essa listagem terá
         override fun getItemCount(): Int {
             return mainItems.size
         }
 
-        //disparo toda vez que houver uma rolagem na tela e for necessario conteudo da celula
-        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-            val itemCurrent = mainItems[position]
-            holder.bind(itemCurrent)
+        // é a classe da celula em si!!!
+        private inner class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: MainItem) {
+                val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+                val name: TextView = itemView.findViewById(R.id.item_txt_name)
+                val container: LinearLayout = itemView.findViewById(R.id.item_container_imc)
 
+                img.setImageResource(item.drawableId)
+                name.setText(item.textStringId)
+                container.setBackgroundColor(item.color)
+
+                container.setOnClickListener {
+
+                    onItemClickListener.invoke(item.id)
+
+
+                }
+            }
         }
 
     }
 
-    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(item: MainItem) {
-           val img: ImageView = itemView.findViewById(R.id.item_img_icon)
-            val name: TextView = itemView.findViewById(R.id.item_txt_name)
-            val container: LinearLayout = itemView as LinearLayout
 
-            img.setImageResource(item.drawableId)
-            name.setText(item.textStringId)
-            container.setBackgroundColor(item.color)
-        }
-    }
 }
